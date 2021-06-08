9232c575add1471e2a98acd00bfff522618e610d
rohitkumar9989 <69589867+rohitkumar9989@users.noreply.github.com>
2021-06-08 08:19:35 +0530

    Added an extra feature hope it helps everyone
    Added the converter 2 function for converting your images into the format you've passed

--- Helperfunction.py
+++ Helperfunction.py
@@ -33,3 +33,40 @@ def converter (original_path, duplicate_path):
     spliter=images[i].split('.')
     path_2=duplicate_path+'/'+f"{spliter[0]}.jpeg"
     cv2.imwrite(path_2, png_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    
+
+    
+    
+"""
+Converting any images to jpeg format ina any directory
+"""
+
+def converter_2 (original_path, extension):
+  """
+  This folder helps in renaming all the images to any extension format in your directory
+  Args*
+  original _path: path which has the images or the directories in it
+  """
+  orig_path=os.listdir(original_path)
+  print (orig_path)
+  if __name__ == "__main__":
+    try:
+      for m in range (len(orig_path)):
+        images=os.listdir(original_path+'/'+orig_path[m])
+        for i in range (len(images)):
+          path=original_path+'/'+orig_path[m]+'/'+images[i]
+          png_img = cv2.imread(path)
+          spliter=images[i].split('.')
+          path_2=original_path+'/'+orig_path[m]+'/'+f"{spliter[0]}.{extension}"
+          os.remove(path=path)
+          cv2.imwrite(path_2, png_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    except Exception as e:
+      for dirpath, dirnames, filenames in os.walk(original_path):
+        files=filenames
+        for l in range (len(filenames)):
+          path2=original_path+'/'+filenames[l]
+          png_img = cv2.imread(path2)
+          spliter=filenames[l].split('.')
+          path_2=original_path+'/'+filenames[l]+'/'+f"{spliter[0]}.jpeg"
+          os.remove(path=path2)
+          cv2.imwrite(path_2, png_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
